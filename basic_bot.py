d5efc3b7c40c14be628adb37a757b7ce60f5551c
Dhruv EC2 <dhruvrrp@gmail.com>
2017-05-30 05:59:25 +0000

    Refactored Usage options

--- basic_bot.py
+++ basic_bot.py
@@ -32,25 +32,31 @@ async def errmsg(message):
     await client.send_message(message.channel, 'Incorrect usage, use -help')
 
 async def usage(message):
-    s = "```Markdown\nUsage\nCheck time for next Boss: !dg_bot basin time [Server group number]\nSet time for Basin Boss: !dg_bot basin set [Server group number] [Minutes since last boss spawn]```\n"
+    s = "```Markdown\nUsage\n\nNew Usage options!\nCheck time for next Boss: !basin [Server group number]\nSet time for Basin Boss: !basin set [Server group number] [Minutes since last boss spawn]```\n"
     await client.send_message(message.channel, s)
    
 async def basin(message, ar):
     s_name = str(message.server.name)
-    s_num = int(ar[3]) - 1
-    if ar[2] == 'set' and len(ar) == 5:
+    if len(ar) == 2:
+        s_num = int(ar[1]) - 1
+    else:
+        s_num = int(ar[2]) - 1
+
+    if ar[1] == 'set' and len(ar) == 4:
+
         logger.warning("Time set for group " + str(s_num+1) + " by " + message.author.name + " for " + s_name)
-        bos_time = int(ar[4]) * 60
+
+        bos_time = int(ar[3]) * 60
         time_left = basin_respawn - bos_time 
         svrs[s_name].next_boss[s_num] = time.mktime(time.localtime()) + time_left
         await client.send_message(message.channel, 'Time left for boss = ' + str(int(time_left/60)) + ' minutes')
-    elif ar[2] == 'time' and len(ar) == 4:
+
+    elif len(ar) == 2:
         if svrs[s_name].next_boss[s_num] == 0:
             await client.send_message(message.channel, 'Respawn time isnt set for this server group!')
             return
         time_left = time.mktime(time.localtime()) - svrs[s_name].next_boss[s_num]
         time_left = basin_respawn - time_left%basin_respawn
-
         at = int(time_left / 60)
         await client.send_message(message.channel, 'Approximate time left for Server Group ' + str(s_num+1) + ' = ' + str(at) + ' minutes')
     else:
@@ -62,19 +68,20 @@ async def on_message(message):
     if s_name not in svrs:
         svrs[s_name] = types.SimpleNamespace()
         svrs[s_name].next_boss = [0]*6
+
     if message.author == client.user:
         return
     if message.content.startswith('!dg_bot'):
+        await usage(message)
+    if message.content.startswith('!basin'):
         ar = message.content.split()
         if len(ar) < 2:
             await client.send_message(message.channel, 'No command entered!') 
-        elif ar[1] == 'basin':
-            if len(ar) == 4 or len(ar) == 5:
-                try:
-                    await basin(message,ar)
-                except ValueError:
-                    await errmsg(message)
-            else:
+        
+        if len(ar) == 2 or len(ar) == 4:
+            try:
+                await basin(message,ar)
+            except ValueError:
                 await errmsg(message)
         else: 
             await usage(message)
